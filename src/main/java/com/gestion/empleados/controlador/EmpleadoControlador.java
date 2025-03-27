be9967169083220d78645cf769e17f4f312cf52a
Robinson Lopez <robins.616009@gmail.com>
2025-03-27 18:28:38 -0500

    Metodo Guardar E

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -3,21 +3,28 @@ package com.gestion.empleados.controlador;
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorio.EmpleadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpleadoControlador {
 
     @Autowired
     private EmpleadoRepositorio repositorio;
 
+    //Lista todos los empleados
     @GetMapping("/empleados")
     public List<Empleado> listarTodosLosEmpleados(){
         return repositorio.findAll();
     }
+
+    //Guarda el Empleado
+    @PostMapping("/empleados")
+    public Empleado guardarEmpleado(@RequestBody Empleado empleado) {
+        return repositorio.save(empleado);
+    }
+
 }
